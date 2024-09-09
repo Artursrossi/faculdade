1dbe56f7b063a73147faf47e26a32a8043ad9e3b
Artur Schincariol Rossi <artursrossi4@gmail.com>
2024-09-09 10:17:03 -0300

    Melhorias na validação de anos bissexto em 'validar-data.c'

--- Algoritmos/validar-data.c
+++ Algoritmos/validar-data.c
@@ -1,50 +1,44 @@
 #include <stdio.h>
 
 int main() {
-    int dd = 0;
-    int mm = 0;
-    int aa = 0;
-    int bisexto = 0;
+    int dd, mm, aa = 0;
+    int bissexto = 0;
 
+    /* Validações de entrada */
     do{
-        printf("Digite o dia: ");
+        printf("Digite o dia:");
         scanf("%d", &dd);
     }
     while(dd <= 0 || dd > 31);
-
     do{
-        printf("Digite o mes: ");
+        printf("Digite o mes:");
         scanf("%d", &mm);
     }
     while(mm <= 0 || mm > 12);
-
     do{
-        printf("Digite o ano: ");
+        printf("Digite o ano:");
         scanf("%d", &aa);
     }
     while(aa <= 0);
-    if((aa % 100 != 0 && aa % 4 == 0) || aa % 400 == 0) bisexto = 1;
 
-    if(mm == 4 || mm == 6 || mm == 9 || mm == 11){
-        if(dd > 30) {
-            printf("Data Invalida.");
-            return 0;
-        }
+
+    /* Verificar se o ano é bissexto */
+    if(aa % 400 == 0 || (aa % 100 != 0 && aa % 4 == 0)) bissexto = 1;
+
+    /* Verificar meses de 30 dias */
+    if((mm == 4 || mm == 6 || mm == 9 || mm == 11) && dd > 30){
+        printf("Data Invalida \n");
+        return 0;
     }
 
-    if(mm == 2){
-        if(!bisexto && dd > 28) {
-            printf("Data Invalida.");
-            return 0;
-        }
-        if(bisexto && dd > 29) {
-            printf("Data Invalida.");
-            return 0;
-        }
+    /* Em fevereiro, se o ano for bissexto, terá 29 dias, se não, terá 28 dias */
+    if((mm == 2 && bissexto && dd > 29) || (mm == 2 && !bissexto && dd > 28)){
+        printf("Data Invalida \n");
+        return 0;
     }
 
-    if(bisexto) printf("O ano digitado eh bisexto! \n");
-    printf("Data Valida: %02d / %02d / %02d",dd,mm,aa);
+    if(bissexto) printf("O ano digitado eh bissexto! \n");
+    printf("A data (%02d/%02d/%02d) eh Valida! \n",dd,mm,aa);
 
     return 0;
 }
